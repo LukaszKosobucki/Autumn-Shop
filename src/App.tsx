889203f1f5ab320187614192985cc7f361d22154
Kosobucki <lukasz.kosobucki@capgemini.com>
2022-09-26 11:53:49 +0200

    imported styles variables from figma, added theme.ts where will be MUI theme. created initial footer and layout, added it to the App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,21 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import BasketPage from "./pages/BasketPage";
 import OrderListPage from "./pages/OrderListPage";
 import FinalizePage from "./pages/FinalizePage";
+import MainLayout from "./components/layout/MainLayout";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<ProductPage />} />
-        <Route path="/basket" element={<BasketPage />} />
-        <Route path="/order-list" element={<OrderListPage />} />
-        {/* think through about dynamic pathing */}
-        <Route path="/finalize" element={<FinalizePage />} />
-      </Routes>
-    </Router>
+    <MainLayout>
+      <Router>
+        <Routes>
+          <Route path="/" element={<ProductPage />} />
+          <Route path="/basket" element={<BasketPage />} />
+          <Route path="/order-list" element={<OrderListPage />} />
+          {/* think through about dynamic pathing */}
+          <Route path="/finalize" element={<FinalizePage />} />
+        </Routes>
+      </Router>
+    </MainLayout>
   );
 }
 
--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,5 +1,53 @@
+import { Box } from "@mui/material";
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexWrap: "wrap",
+        px: 10,
+        py: 4,
+        bgcolor: "#3d405b",
+        color: "#cac7b5",
+        justifyContent: "space-around",
+        alignItems: "center",
+      }}
+    >
+      <Box>
+        <Box borderBottom={1}>
+          <p>hello world</p>
+        </Box>
+        <Box>
+          <p>hello world</p>
+        </Box>
+        <Box>
+          <p>hello world</p>
+        </Box>
+      </Box>
+      <Box>
+        <Box borderBottom={1}>
+          <p>hello world</p>
+        </Box>
+        <Box>
+          <p>hello world</p>
+        </Box>
+        <Box>
+          <p>hello world</p>
+        </Box>
+      </Box>
+      <Box>
+        <Box borderBottom={1}>
+          <p>hello world</p>
+        </Box>
+        <Box>
+          <p>hello world</p>
+        </Box>
+        <Box>
+          <p>hello world</p>
+        </Box>
+      </Box>
+    </Box>
+  );
 };
 
 export default Footer;
