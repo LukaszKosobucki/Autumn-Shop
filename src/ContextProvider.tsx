c5a1b3429e58cca1577fb6f5db1ef2fbe5df441d
Kosobucki <lukasz.kosobucki@capgemini.com>
2022-09-27 15:54:37 +0200

    added initial code to the contextProvider

--- src/ContextProvider.tsx
+++ src/ContextProvider.tsx
@@ -1,7 +1,13 @@
-import React from "react";
+import React, { createContext, useState } from "react";
 
-export {};
+export const dataContext = createContext({});
 
+const ContextProvider = ({ children }: any) => {
+  const [data, setData] = useState<any>([]);
+  const values = [data];
+  return <dataContext.Provider value={values}>{children}</dataContext.Provider>;
+};
+export default ContextProvider;
 /**
  * zrób content provider dla danych i może dodaj do niego jakieś state'ty co by zrobić redux dla ubogich
  */
